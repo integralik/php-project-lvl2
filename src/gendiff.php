2d0fade097297b382061e37b9eaa1d3ccfa8816c
integralik <integralik@gmail.com>
2020-09-10 00:04:11 +0300

    added format and path to file

--- src/gendiff.php
+++ src/gendiff.php
@@ -7,15 +7,18 @@ function gendiff()
 //	\cli\line('!!!');
 
 	$doc = <<<DOC
+
 Generate diff
 
 Usage:
   gendiff (-h|--help)
   gendiff (-v|--version)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
 
 Options:
   -h --help                     Show this screen
   -v --version                  Show version
+  --format <fmt>                Report format [default: stylish]
 DOC;
 
 	$args = \Docopt::handle($doc, array('version'=>'0.0.1'));
